83ac37f9b366640d20b70ce724230a881abf663b
Zachary Auker <zachary.auker@gmail.com>
2023-09-24 16:55:19 -0400

    Preserving work...

---
+++ Persistence/Seeders/CompanySeed.cs
@@ -0,0 +1,30 @@
+using IGDB;
+using DomainCompany = Domain.Entities.Company;
+using ApiCompany = IGDB.Models.Company;
+namespace Persistence.Seeders;
+
+public class CompanySeed
+{
+    public static async Task SeedData(DataContext context)
+    {
+        if (context.Companies.Any()) return;
+        
+        var igdb = new IGDBClient("3p2ubjeep5tco48ebgolo2o4a1cjek", "9lzmwsx9eai2lkqg1ye5waxovtltrp");
+
+        var apiCompanies = await igdb.QueryAsync<ApiCompany>(IGDBClient.Endpoints.Companies, "fields *; limit 250;");
+        foreach (var apiCompany in apiCompanies)
+        {
+            var company = new DomainCompany
+            {
+                Name = apiCompany.Name,
+                Slug = apiCompany.Slug,
+                Description = apiCompany.Description,
+                IgdbId = apiCompany.Id
+            };
+            
+            await context.Companies.AddRangeAsync(company);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
